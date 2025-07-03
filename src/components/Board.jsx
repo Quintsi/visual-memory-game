f3060e2fcec6b1ff0b233aad6dc683f6007a6d2b
Quintsi <Evan.N.Sharp@gmail.com>
2025-07-02 22:41:57 -0400

    comment

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,6 +5,7 @@ export const Board = () => {
     const [gridSize, setGridSize] = useState(5);
 
     // Should have a resizeable grid layout that can flash white and user can select their picks at which cell flashed
+    // I have no idea how to get a gird layout...
     return (
         <div>
             <input 
